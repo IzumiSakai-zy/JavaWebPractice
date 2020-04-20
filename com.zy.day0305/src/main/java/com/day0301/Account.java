0f0f4b19f2bae49661d3b1481d714d6c0656b371
IzumiSakai <542270191@qq.com>
2020-04-20 23:24:11 +0800

    0305

---
+++ com.zy.day0305/src/main/java/com/day0301/Account.java
@@ -0,0 +1,8 @@
+package com.day0301;
+
+public class Account{
+    public Account(){System.out.println("调用了构造方法");}
+    public void printHello() {
+        System.out.println("测试Account类成功");
+    }
+}

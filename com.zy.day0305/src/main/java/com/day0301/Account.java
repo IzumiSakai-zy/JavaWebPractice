dd1d695c9a885099c6f934b6f0443649ec29f5e0
origin <izumisakai@aliyun.com>
2020-07-01 14:07:52 +0800

    删除前期spring内容部分

--- com.zy.day0305/src/main/java/com/day0301/Account.java
+++
@@ -1,8 +0,0 @@
-package com.day0301;
-
-public class Account{
-    public Account(){System.out.println("调用了构造方法");}
-    public void printHello() {
-        System.out.println("测试Account类成功");
-    }
-}
